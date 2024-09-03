9a9b994dcf7217eebe590cab668f0fe4e7ac3a87
Kavindu <kavinduinduwara100@gmail.com>
2024-09-03 21:55:05 +0530

    converted data into numbers

---
+++ Itemsetmining/mining_functions.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Find the maximum value in a column
+def get_max_and_min(column_name, df):
+    maximum_value = df[column_name].max()
+
+    # Find the minimum value in a column
+    minimum_value = df[column_name].min()
+
+    # Print the maximum and minimum values
+    print("Maximum Value:", maximum_value)
+    print("Minimum Value:", minimum_value)
+
+
+# Function to categorize values based on ranges
+def categorize_value(value, ranges):
+    # if pd.isnull(value):
+    # return 'unknown'  # Treat NaN values as 'unknown'
+    for category, (lower, upper) in ranges.items():
+        if lower <= value <= upper:
+            return category
+    # return 'unknown'
+
+
+def adaptive_bin_handling(column_name, df, bin_data):
+    bin_ranges, bin_edges = pd.qcut(df[f'{column_name}'], 10, labels=None, retbins=True, precision=2, duplicates='drop')
+    print(bin_ranges.value_counts())
+
+    ranges = {}
+    for idx in range(len(bin_edges) - 1):
+        ranges.update({f"{(bin_edges[idx] + bin_edges[idx + 1]) / 2}": (bin_edges[idx], bin_edges[idx + 1])})
+
+    bin_data.append([column_name, ranges])
+
+    # Apply categorization to 'Flow Duration' column
+    df[f'{column_name} Category'] = df[f'{column_name}'].apply(lambda x: categorize_value(x, ranges))
+    print(df[f'{column_name} Category'].value_counts())
+
+    # plt.figure(figsize=(20, 8))
+    # sns.histplot(data=df, x=df[f'{column_name} Category'], hue='Label', multiple='stack', discrete='True')
+    # plt.show()
+
+    print("\n================================================================================\n")
+
+    return f"{column_name} Category", bin_data
--- Itemsetmining/mining_patterns_charm.py
+++ Itemsetmining/mining_patterns_charm.py
@@ -7,7 +7,7 @@ import numpy as np
 from sklearn.preprocessing import OrdinalEncoder
 import joblib
 import json
-from mining_functions import get_max_and_min, adaptive_bin_handling
+from mining_functions import adaptive_bin_handling
 
 df = pd.read_csv('NewDataSet.csv')
 
@@ -48,5 +48,100 @@ for col in selected_features:
 essential_columns = []
 
 for to_be_handled_column_names in handled_columns:
-    category_name = adaptive_bin_handling(to_be_handled_column_names, df, bin_data)
+    category_name, bin_data = adaptive_bin_handling(to_be_handled_column_names, df, bin_data)
     essential_columns.append(category_name)
+
+other_columns = ['Dst Port','Protocol']
+essential_columns = essential_columns + other_columns + unhandled_columns
+
+# Drop non-essential columns
+filtered_df = df[essential_columns]
+print("\nFiltered dataset Summery")
+print(filtered_df.info())
+
+categorical_cols = filtered_df.select_dtypes(include=['object','category']).columns
+numerical_cols = filtered_df.select_dtypes(include=['int64']).columns
+
+# Encode categorical features
+encoder = OrdinalEncoder()
+encoder.fit_transform(filtered_df[categorical_cols])
+# Save the encoder and the model
+
+for i, category in enumerate(encoder.categories_):
+    if np.nan not in category:
+        encoder.categories_[i] = np.append(category, np.nan)
+
+joblib.dump(encoder, 'feature_encoder.pkl')
+
+with open('categorization.json', 'w') as file:
+    json.dump(bin_data, file)
+
+# Save the column order
+column_order = filtered_df.columns.tolist()
+with open('column_order.json', 'w') as file:
+    json.dump(column_order, file)
+
+with_ID = filtered_df.copy()
+with_ID.insert(0, 'alertID', range(len(with_ID)))
+print("\nAdding a ID number for the records")
+print(with_ID.head())
+
+dataset_with_ID = with_ID.values.tolist()
+
+print("\nconverting the dataset in to a list")
+dataset = [alert[1:] for alert in dataset_with_ID]
+
+# Convert each sublist to a tuple for hash ability
+data_tuples = [tuple(sublist) for sublist in dataset]
+
+# Count occurrences of each unique record
+record_counts = Counter(data_tuples)
+
+print("\nnumber occurrences of each unique record")
+print(f"{len(record_counts) }")
+
+# Assuming 'filtered_df' is your DataFrame with categorical values
+
+# Initialize dictionaries to store both forward and reverse mappings
+forward_mapping = {}
+reverse_mapping = {}
+
+# Initialize the global counter to keep track of numerical values
+global_counter = 0
+
+# Iterate over each column in the DataFrame
+for column in filtered_df.columns:
+    # Initialize a local counter for each column
+    local_counter = 0
+
+    # Initialize dictionaries for forward and reverse mappings for the current column
+    forward_mapping[column] = {}
+    reverse_mapping[column] = {}
+
+    # Iterate over each unique value in the current column
+    for value in filtered_df[column].unique():
+        # Map each unique value to a numerical value based on the global counter
+        forward_mapping[column][value] = global_counter + local_counter
+        reverse_mapping[column][global_counter + local_counter] = value
+
+        # Increment the local counter
+        local_counter += 1
+
+    # Update the global counter to continue numbering from the last value of the previous dictionary
+    global_counter += local_counter
+
+# Create a new DataFrame with mapped values
+new_df = pd.DataFrame()
+
+# Iterate over each column in the original DataFrame and fill in values in the new DataFrame
+for column in filtered_df.columns:
+    new_df[column] = filtered_df[column].map(forward_mapping[column])
+
+combined_dict = {k: v for inner_dict in reverse_mapping.values() for k, v in inner_dict.items()}
+
+item_dataset = [tuple(x) for x in new_df.to_records(index=False)]
+
+# Add ID field to the start of each tuple
+item_dataset_withID = [(i,) + record for i, record in enumerate(item_dataset)]
+
+new_df.to_csv('data.txt', index=False, sep=' ', header=False)
